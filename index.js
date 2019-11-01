0643b87a14e218742d8e9328fdf966dc9c2e1024
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2019-11-01 10:17:48 +0100

    written an implementation for get method. unit tests are still missing there

--- index.js
+++ index.js
@@ -11,6 +11,46 @@ class LinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  get(index) {
+    if (!Number.isInteger(index)) {
+      throw new TypeError("Index must be an integer number");
+    }
+
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+
+    const answer = this._find(index, this._testByIndex);
+    return answer;
+  }
+
+  _find(searchedValue, testPredicate) {
+    let currentNode = this.head;
+    let currentIndex = 0;
+
+    while (currentNode) {
+      if (
+        testPredicate(
+          searchedValue,
+          currentNode.value,
+          currentIndex,
+          currentNode
+        )
+      ) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+
+    return null;
+  }
+
+  _testByIndex(searchedValue, _, currentIndex, __) {
+    return searchedValue === currentIndex;
+  }
 }
 
 exports.Node = Node;
